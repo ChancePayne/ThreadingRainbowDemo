e4b84f929713ca5f15c62434c30b963591460a1e
Chance Payne <chance@lambdaschool.com>
2018-11-26 11:18:13 -0700

    add multiple version of threads

--- app/src/main/java/com/lambdaschool/threadingrainbowdemo/MainActivity.java
+++ app/src/main/java/com/lambdaschool/threadingrainbowdemo/MainActivity.java
@@ -3,21 +3,26 @@ package com.lambdaschool.threadingrainbowdemo;
 import android.animation.ArgbEvaluator;
 import android.animation.ValueAnimator;
 import android.graphics.drawable.TransitionDrawable;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final int TRANSITION_DURATION = 500;
-    View parent;
-    int  colorCode;
+    View     parent;
+    TextView textView;
+    int      colorCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.output_text);
 
         colorCode = 0;
         parent = findViewById(R.id.parent_main);
@@ -25,9 +30,97 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 changeParentBackgroundColor();
-
             }
         });
+        new Task().execute();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        /*String text = "";
+        for(int i = 0; i < 10000; ++i) {
+            text += i;
+        }
+        Log.i(getPackageName(), text);*/
+
+//        exceptionCheck();
+
+
+        /*Thread task = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String text = "";
+                for(int i = 0; i < 10000; ++i) {
+                    text += i;
+                    *//*try {
+                        Thread.sleep(250);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }*//*
+                }
+                final String finalText = text;
+                Log.i(getPackageName(), text);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        ((TextView)findViewById(R.id.output_text)).setText(finalText);
+                        changeParentBackgroundColor();
+                    }
+                });
+            }
+        });
+        task.start();*/
+        /*try {
+            task.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
+    }
+
+    private int exceptionCheck() {
+        Integer num = null;
+        if(num != null) {
+            return num.byteValue();
+        } else {
+            return 0;
+        }
+    }
+
+    public class Task extends AsyncTask<Void, Integer, String> {
+        @Override
+        protected void onPreExecute() {
+            findViewById(R.id.progress_bar).setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            String text = "";
+            for(int i = 0; i < 100; ++i) {
+                text += i;
+                publishProgress(i);
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return text;
+        }
+
+        @Override
+        protected void onPostExecute(String finalText) {
+            findViewById(R.id.progress_bar).setVisibility(View.GONE);
+            textView.setText(finalText);
+            changeParentBackgroundColor();
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            textView.setText(values[0].toString());
+            super.onProgressUpdate(values);
+        }
     }
 
     private void changeParentBackgroundColor() {
